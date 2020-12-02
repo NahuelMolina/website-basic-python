deeb9aac27529a6faea775892a7072ee3a05d6e8
_moliNasil <41929823+NahuelMolina@users.noreply.github.com>
2020-12-02 13:14:55 -0300

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birthdate = models.DateField(null=True)
+    email = models.CharField(max_length=190)
+
+
+class Pets(models.Model):
+    Owner_id = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='%(class)s_Owner_id', null=True)
+    name = models.CharField(max_length=20)
+    age = models.IntegerField(null=True)
+
+
+class Devices(models.Model):
+    model = models.CharField(max_length=100)
+    operating_system = models.CharField(max_length=100)
+    api_token = models.CharField(max_length=191)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='%(class)s_user_id', null=True)
+
+
+
